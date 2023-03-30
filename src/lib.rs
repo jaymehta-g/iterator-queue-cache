f634a36e68ef770e3b34db3ba871107101563501
Jay Mehta <91770194+jaymehta-g@users.noreply.github.com>
2023-03-30 18:39:58 -0500

    b4 big renaming

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,9 @@ mod tests;
 extern crate queues; 
 use queues::*;
 use std::{error::*, fmt::Display};
-/**
- * Utility
- */
+/// Utility
 type BoxResult<T> = Result<T,Box<dyn Error>>;
-/**
- * When `enqueue()` is called but the iterator is empty
- */
+/// Error thrown when `enqueue()` is called but the iterator is empty
 #[derive(Debug)]
 pub struct EnqueueWhileIteratorIsEmptyError;
 impl Display for EnqueueWhileIteratorIsEmptyError {
@@ -19,11 +15,9 @@ impl Display for EnqueueWhileIteratorIsEmptyError {
     }
 }
 impl Error for EnqueueWhileIteratorIsEmptyError {}
-/**
- * An iterator that 'unlazies your evaluation' by caching items from your iterator now to be used later
- * Extract a certain amount of items from any iterator into a cache with the `queue()` method
- * When extracting from the resulting iterator, items will first be pulled from cache until it is exhausted 
- */
+/// An iterator that 'unlazies your evaluation' by caching items from your iterator now to be used later. 
+/// Extract a certain amount of items from any iterator into a cache with the `queue()` method.
+/// When extracting from the resulting iterator, items will first be pulled from cache until it is exhausted .
 pub struct IteratorQueueCache<T: Clone>
 where
     Queue<T>: Default, // Queue has trait bound for T
@@ -35,6 +29,8 @@ impl<T> IteratorQueueCache<T>
 where
     T: Clone,
 {
+    /// Create a new cache with an iterator.
+    /// Identical to `iterator.queue(0).unwrap()` or `iterator.queue_or_all(0)` 
     pub fn new(iter: impl Iterator<Item = T> + 'static) -> Self {
         IteratorQueueCache {
             queue: Queue::default(),
@@ -42,9 +38,12 @@ where
         }
     }
     // methods
-    /**
-     * Lets the struct cache further items after its creation
-     */
+    /// Caches more items from the contained iterator.
+    /// # Example:
+    /// ```
+    /// let cache = (0..5).queue(2); // Caches 2 values
+    /// cache.enqueue(2); // Caches 2 more values for a total of 4
+    /// cache.for_each(|i| println!("{}",i)); // Exhausts the 4 values in the cache, then prints the last item directly out of the iterator
     pub fn enqueue(&mut self, quantity: u32) -> BoxResult<()> {
         for _ in 0..quantity {
             if let Some(item) = self.iterator.next() {
@@ -74,12 +73,15 @@ where
         return self.iterator.next();
     }
 }
+/// Any Iterator can have its values cached with the methods in this trait.
+/// (Currently only iterators where Item: Clone are supported, due to trait bounds on the queue from the `queues` crate)
 pub trait IterateIntoQueue: Iterator
     where Self::Item: Clone 
 {
-    /**
-     * Cache a given amount of items from this iterator for use later
-     */
+    /// Cache a given amount of items from this iterator for use later.
+    /// # Errors
+    /// Errors if the iterator runs out of items while caching
+    /// To avoid the error, consider using `queue_or_all()`
     fn queue(self, quantity: u32) -> BoxResult<IteratorQueueCache<Self::Item>>;
     /**
      * Cache all items from this iterator for use later
